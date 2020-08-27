9c0ab958ff536087cdf536b3ff7abd497cc1dce5
Brandon <brandon.amaral9658@gmail.com>
2020-08-26 22:14:27 -0300

    Added User forms Added SignUp view Added signup page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,2 +1,23 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
+
+
+class AccountCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+    
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['username'].label = 'Usuário'
+        self.fields['email'].label = 'E-mail'
+        self.fields['email'].widget.attrs.pop("autofocus", None)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from .forms import AccountCreationForm
 
-# Create your views here.
+
+class SignUp(FormView):
+    template_name = 'registration/signup.html'
+    form_class = AccountCreationForm
+    success_url = reverse_lazy('login')
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
